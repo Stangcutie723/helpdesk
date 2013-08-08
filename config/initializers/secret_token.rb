43a9226fbd0df8e557f06723bf90284417a110b5
Stangcutie723 <Stangcutie723@gmail.com>
2013-08-07 17:35:34 -0700

    creating db helpdesk

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helpdesk::Application.config.secret_key_base = '794527885a8b510e6db27b37bd12789106148b5da5f442b79a8dab6f63d5fa525d923b1ee2bd7ae5962443727e5c354c7effdb69cefeb0f41e5f76e61cb24e1b'
