e8df4ba1dfd5c24d46b46cfb1e23a39c4c5d062e
Stangcutie723 <Stangcutie723@gmail.com>
2013-08-07 18:02:55 -0700

    setting up gems and addons

--- Gemfile
+++ Gemfile
@@ -2,6 +2,12 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
+gem 'mail'
+
+gem 'postmark-rails'
+gem 'postmark'
+
+
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.action_mailer.delivery_method = :postmark
+config.action_mailer.postmark_settings = {:api_key => ENV['POSTMARK_API_KEY']}
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
